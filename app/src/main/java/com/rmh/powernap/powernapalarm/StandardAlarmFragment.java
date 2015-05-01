f394687a5bbcf0593447ed743f121aa6d31cbed3
ernieb4768 <hoffmanryan1@gmail.com>
2015-04-30 23:38:34 -0400

    Card UI update

--- app/src/main/java/com/rmh/powernap/powernapalarm/StandardAlarmFragment.java
+++ app/src/main/java/com/rmh/powernap/powernapalarm/StandardAlarmFragment.java
@@ -5,8 +5,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.special.ResideMenu.ResideMenu;
+
+import java.util.ArrayList;
+
+import it.gmariotti.cardslib.library.cards.actions.BaseSupplementalAction;
+import it.gmariotti.cardslib.library.cards.actions.TextSupplementalAction;
+import it.gmariotti.cardslib.library.cards.material.MaterialLargeImageCard;
+import it.gmariotti.cardslib.library.internal.Card;
+import it.gmariotti.cardslib.library.view.CardViewNative;
 
 
 
@@ -22,17 +31,55 @@ public class StandardAlarmFragment extends Fragment {
 
 	private View parentView;
 	private ResideMenu resideMenu;
+	private CardViewNative cardViewNative;
+
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		parentView = inflater.inflate(R.layout.fragment_standard_alarm, container, false);
 		setUpViews();
+		buildMaterialCards();
 		return parentView;
 	}
 
 	private void setUpViews() {
 		MainActivity parentActivity = (MainActivity) getActivity();
 		resideMenu = parentActivity.getResideMenu();
+	}
+
+	private MaterialLargeImageCard buildMaterialCards(){
+
+		ArrayList<BaseSupplementalAction> actions = new ArrayList<>();
+		TextSupplementalAction t1 = new TextSupplementalAction(getActivity(), R.id.text1);
+		t1.setOnActionClickListener(new BaseSupplementalAction.OnActionClickListener() {
+
+			@Override
+			public void onClick(Card card, View view) {
+
+				Toast.makeText(getActivity(), "Alarm is set", Toast.LENGTH_SHORT).show();
+			}
+		});
+		actions.add(t1);
+		TextSupplementalAction t2 = new TextSupplementalAction(getActivity(), R.id.text2);
+		t2.setOnActionClickListener(new BaseSupplementalAction.OnActionClickListener() {
+
+			@Override
+			public void onClick(Card card, View view) {
+
+				Toast.makeText(getActivity(), "Alarm is cancelled", Toast.LENGTH_SHORT).show();
+			}
+		});
+
+		MaterialLargeImageCard card = MaterialLargeImageCard.with(getActivity())
+				.setTextOverImage("10 minutes")
+				.useDrawableId(R.drawable.card_background)
+				.setupSupplementalActions(R.layout.card_native_material_supplemental_actions_large, actions)
+				.build();
+
+		cardViewNative = (CardViewNative) this.parentView.findViewById(R.id.materialCard);
+		cardViewNative.setCard(card);
+
+		return card;
 
 	}
 
